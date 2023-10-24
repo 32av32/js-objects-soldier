f108f21f7811bc955952de8378d9b4f62dff8746
Isa Amerkhanov <isa.amerkhanov@gmail.com>
2023-10-24 09:40:57 +0300

    Реализованы функции объекта soldier

--- soldier.js
+++ soldier.js
@@ -6,7 +6,25 @@ const soldier = {
         bullets: 30
     },
     ammo: 3,
-    fire: function () {},
-    reload: function () {},
-    hurt: function () {},
+    fire: function () {
+        if (this.weapon.bullets) {
+            console.log('бах-бах')
+            this.weapon.bullets--
+        } else {
+            console.log('обойма пуста. Перезарядитесь')
+        }
+
+    },
+    reload: function () {
+        if (this.ammo) {
+            console.log('перезарядка...')
+            this.weapon.bullets = 30
+            this.ammo--
+        } else {
+            console.log('не осталось припасов')
+        }
+    },
+    hurt: function () {
+        this.health ? this.health-- : console.log('Ты проиграл')
+    },
 }
